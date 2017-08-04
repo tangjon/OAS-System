bb038fa8fa24a70de0bd541a153cb9f94d45d7dd
Jonathan C. Tang <JCTang@bcscnet.ca>
2017-08-04 12:05:50 -0700

    test first base

---
+++ js/script.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize Firebase
+    // Firebase config
+    var config = {
+        apiKey: "AIzaSyCYmEr20lO0yqLWtOIZCcmu3Ql354-1e30",
+        authDomain: "myawesomeproject-783f9.firebaseapp.com",
+        databaseURL: "https://myawesomeproject-783f9.firebaseio.com",
+        projectId: "myawesomeproject-783f9",
+        storageBucket: "myawesomeproject-783f9.appspot.com",
+        messagingSenderId: "289250326133"
+    };
+    firebase.initializeApp(config);    
+    var db = firebase.database();
+    var auth = firebase.auth();
+
+    var bigOne = document.getElementById('bigOne');
+    var dbRef = firebase.database().ref().child('Text');
+    dbRef.on('value', snap=> bigOne.innerText = snap.val())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
