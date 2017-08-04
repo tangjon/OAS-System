25b359f015682e3fa5c15e464f24d1ab85da0a19
Jonathan C. Tang <JCTang@bcscnet.ca>
2017-08-04 09:36:12 -0700

    Initial commit, update the config.yml :

---
+++ js/myScript.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.skills', {
+    strings: [
+        "Programmer",
+        "Web Development",
+        "Student",
+        "Youth Volunteer"
+    ],
+    typeSpeed: 90,
+    backDelay: 200,
+    loop: true
+});
