77394492746f3f60b3437a65fad6d0481833a7ac
Jonathan Tang <tangjon@hotmail.com>
2017-09-12 00:11:13 -0700

    Table is now configured for sorting fixed headers only

---
+++ js/list.js
@@ -0,0 +1,12 @@
+
+window.onload = function () {
+
+}
+
+$(document).ready(function () {
+    var options = {
+        valueNames: ['material', 'quantity', 'price']
+    }
+        , documentTable = new List('mdl-table', options)
+        ;
+});
--- js/oasTable.js
+++ js/oasTable.js
@@ -1,100 +1,206 @@
-var data = (function(){
+var data = (function () {
     var memberList;
+    var fixedHeaders = ['Level', 'Last', 'First', 'Section'];
 
     // Subscribe to changes on memberlist
     events.on('updatedMembers', updateMemberList);
 
     // Fetch member objects
-    function updateMemberList(value){
+    function updateMemberList(value) {
         memberList = value;
     }
 
-    function getMemberList(){
+    function getMemberList() {
         return memberList;
     }
-    
+
+    function getFixedHeaders() {
+        return fixedHeaders;
+    }
+
     return {
-        getMemberList : getMemberList
+        getMemberList: getMemberList,
+        getFixedHeaders: getFixedHeaders
     }
 
 })();
+
+
 
 
 var oasTable = (function () {
 
     // Cache dom
     var tbl = $("#my-badge-table");
-    var section;
- 
+    var table = doc.getElementById("my-badge-table");
+    var view;
+    var initailized = false;
 
-     // Subscribe to changes on memberlist
+
+    // Subscribe to changes on memberlist
     events.on('updatedMembers', render)
 
-    function buildHeader(){
-        var tblHeaders = [];
-        db.ref('meta/headers').once("value", function (snapshot) {
-            var snap = snapshot.val();
-            for (var key in snap) {
-                tblHeaders.push(snap[key]);
-            }
-        });
+    function generateHeader() {
+        // var tblHeaders = [];
+        // db.ref('meta/headers').once("value", function (snapshot) {
+        //     var snap = snapshot.val();
+        //     for (var key in snap) {
+        //         tblHeaders.push(snap[key]);
+        //     }
+        // });
         // TODO : Currently only reading from scout badges...
+        var thead = tbl.find('thead');
+        var tr = $('<tr></tr>');
         db.ref('badge_info/scout_badges').once("value", function (snapshot) {
-            var snap = snapshot.val();
-            for (var key in snap) {
-                tblHeaders.push(snap[key].name);
+            var fixedHeaders = data.getFixedHeaders();
+            fixedHeaders.forEach(function (element) {
+                tr.append($('<th>' + element + '</th>').attr('class', 'mdl-data-table__cell sort').attr('data-sort', element.toLowerCase()));
+            }, this);
+            var object = snapshot.val();
+            for (var key in object) {
+                if (object.hasOwnProperty(key)) {
+                    var element = object[key].name;
+                    tr.append($('<th>' + element + '</th>'));
+                }
             }
+            thead.append(tr);
+            tbl.append(thead);
         });
-        console.log(tblHeaders);
-        return tblHeaders;
     }
 
-    function buildSpinner(member){
-         // GENERATE DROP DOWNS
-         var select = document.createElement("SELECT");
-         select.setAttribute("class", "mdl-selectfield__select");
-         select.setAttribute("id", key + ' ' + badge)
-         select.setAttribute("onchange", "handleSelectBox(this);");
-
-         // Spinner Values
-         if (member.section != "beaver") {
-             for (var i = 0; i <= 9; i++) {
-                 var opt = document.createElement('OPTION');
-                 opt.value = i;
-                 opt.innerHTML = i;
-                 select.appendChild(opt);
-             }
-         } else {
-             for (var i = 0; i <= 3; i++) {
-                 var opt = document.createElement('OPTION');
-                 opt.value = i;
-                 opt.innerHTML = i;
-                 select.appendChild(opt);
-             }
-         }
-         // Set Spinner value to current member level
-         select.value = thisBadge.level;
-         return select;
-    }
-    function createTable(){
-     
+    function generateBody() {
         var members = data.getMemberList();
+        var tbody = tbl.children('tbody');
+        for (var key in members) {
+            if (members.hasOwnProperty(key)) {
+                var member = members[key];
+                // Insert Rows with id
+                var tr = $('<tr></tr>').attr('id', key);
+                // OAS LEVEL
+                tr.append($('<td></td>').attr('class', 'mdl-data-table__cell level').attr('data-sort', 'level').append(calculateOASLevel(member)));
+                // MEMBER LAST 
+                tr.append($('<td></td>').attr('class', 'last').append(member.lname));
+                // MEMBER FIRST
+                tr.append($('<td></td>').attr('class', 'first').append(member.fname));
+                // MEMBER SECION
+                var sectionBadge = $('<div></div>').attr('class', 'section-banner ' + 'section-banner--' + member.section)
+                    .append(member.section.toUpperCase());
+                tr.append($('<td></td>').attr('class', 'section').append(sectionBadge));
+
+                // Decide which badges to show
+                var badge_catalogue;
+                switch (member.section) {
+                    case 'beaver':
+                        badge_catalogue = member.beaver_badges;
+                        break;
+                    case 'cub':
+                        badge_catalogue = member.cub_badges;
+                        break;
+                    case 'scout':
+                        badge_catalogue = member.scout_badges;
+                        break;
+                }
+                for (var badgekey in badge_catalogue) {
+                    if (badge_catalogue.hasOwnProperty(badgekey)) {
+                        var badge = badge_catalogue[badgekey];
+                        // Setup SELECT
+                        var select = $('<select></select>').attr({
+                            class: 'mdl-selectfield__select',
+                            id: key + ' ' + badgekey,
+                            onchange: 'handleSelectBox(this)'
+                        });
+                        // Spinner Values
+                        if (member.section != "beaver") {
+                            for (var i = 0; i <= 9; i++) {
+                                var opt = $('<option></option>');
+                                opt.val(i);
+                                opt.append(i);
+                                select.append(opt);
+                            }
+                        } else {
+                            for (var i = 0; i <= 3; i++) {
+                                var opt = $('<option></option>');
+                                opt.val(i);
+                                opt.append(i);
+                                select.append(opt);
+                            }
+                        }
+                        // Set Spinner value to current member level
+                        select.val(badge.level);
+                        tr.append($('<td></td>').attr('class', badgekey).append(select));
+                    }
+                }
+
+
+                tbody.append(tr);
+            }
+        }
+        tbl.append(tbody);
+    }
+
+    function buildSpinner(member, key, badgekey) {
+        // GENERATE DROP DOWNS
+        // var select = document.createElement("SELECT");
+
+
+        // select.setAttribute("class", "mdl-selectfield__select");
+        // select.setAttribute("id", key + ' ' + badge)
+        // select.setAttribute("onchange", "handleSelectBox(this);");
+
+
+        return select;
+
+
+    }
+    function calculateOASLevel(member) {
+
+        var oas_level = 0;
+        var member_badge_catalogue;
+        var list = [];
+        if (member.beaver_badges) {
+            list.push(member.beaver_badges);
+        }
+        if (member.cub_badges) {
+            list.push(member.cub_badges);
+        }
+        if (member.scout_badges) {
+            list.push(member.scout_badges);
+        }
+
+        list.forEach(function (element) {
+            for (var badge in element) {
+                oas_level += parseInt(element[badge].level);
+            }
+        }, this);
+
+        return oas_level;
+    }
+
+    function render() {
 
         // Generate Header
-        var thead = $('<thead></thead>');
-        var tblheaders = buildHeader();
-        console.log(tblheaders[0]);
-        tbl.append(thead);
-        
-        // Iterate through members
-        for(var key in members){
-            var currMember = members[key];
-            
-        }
-    }
-    function render() {
-        createTable();
-            
+        generateHeader();
+        setTimeout(generateBody, 100);
+        setTimeout(function () {
+            var lowHeaders = lowerArr(data.getFixedHeaders());
+            var boptions = {
+                valueNames: lowHeaders
+            }, documentTable = new List('my-badge-table', boptions);
+
+            function lowerArr(arr){
+                var lower = [];
+                arr.forEach(function(element) {
+                    lower.push(element.toLowerCase());
+                }, this);
+                return lower;
+            }
+        }, 1);
+        // generateBody();
+
+
     }
 
 })();
+
+
+
